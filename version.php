f36d580aee99751450c2819b6f20c7b514a1ef73
HannesFunk <42032797+HannesFunk@users.noreply.github.com>
2025-11-04 19:13:57 +0100

    Bump version

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 /** @var stdClass $plugin */
 $plugin->component = 'mod_vocabcoach';
 $plugin->release = '0.1.0';
-$plugin->version = 2023121613;
+$plugin->version = 2025100121;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
